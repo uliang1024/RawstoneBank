8c90f620276d05ce154416559585b42d34b32410
uliang <amisleo000@gmail.com>
2023-06-15 17:54:21 +0800

    好啦這個頁面就這樣(不改了)

--- js/upload_docs.js
+++ js/upload_docs.js
@@ -1,6 +1,7 @@
-const cardFiles = document.querySelectorAll(".card-file");
-const fileInputs = document.querySelectorAll(".file-input");
-const uploadedAreas = document.querySelectorAll(".uploaded-area");
+let cardFiles = document.querySelectorAll(".card-file");
+let fileInputs = document.querySelectorAll(".file-input");
+let uploadedAreas = document.querySelectorAll(".uploaded-area");
+let fileImg = document.querySelectorAll(".file-img");
 
 // 为每个卡片绑定点击事件
 cardFiles.forEach((cardFile, index) => {
@@ -12,6 +13,7 @@ cardFiles.forEach((cardFile, index) => {
 // 监听文件选择事件
 fileInputs.forEach((fileInput, index) => {
   fileInput.onchange = ({ target }) => {
+    let success = true;
     let file = target.files[0];
     if (file) {
       let fileName = file.name;
@@ -31,8 +33,8 @@ fileInputs.forEach((fileInput, index) => {
       } else if (fileSize < 30 * 1024 * 1024) {
         fileSizeString = (fileSize / (1024 * 1024)).toFixed(2) + " MB"; // 小于30MB，以MB为单位，保留两位小数
       } else {
-        console.log("文件大小超过30MB");
-        return;
+        fileName = "文件大小超过30MB";
+        success = false;
       }
 
       // 检查文件格式和文件名
@@ -40,19 +42,21 @@ fileInputs.forEach((fileInput, index) => {
       let fileFormat = fileName.split(".").pop().toLowerCase();
 
       if (!allowedFormats.includes(fileFormat)) {
-        console.log("上传文件格式限制为 JPG、JPEG");
-        return;
+        fileName = "上传文件格式限制为 JPG、JPEG";
+        success = false;
       }
 
-      console.log(fileSizeString); // 显示文件大小
+      let imageURL = URL.createObjectURL(file); // 获取图像的 URL
 
-      uploadFile(fileName, fileSizeString, index);
+      uploadFile(fileName, fileSizeString, imageURL, index, success);
     }
   };
 });
 
-function uploadFile(name, size, index) {
-  let uploadedHTML = `<li class="row">
+function uploadFile(name, size, image, index, success) {
+  let uploadedHTML;
+  if (success) {
+    uploadedHTML = `<li class="row" style="background: #e9f0ff;">
                         <div class="col-2"><i class="fas fa-file-alt"></i></div>
                         <div class="col-10">
                           <div class="content">
@@ -60,10 +64,27 @@ function uploadFile(name, size, index) {
                               <span class="name">${name}．Uploaded</span>
                               <span class="size">${size}</span>
                             </div>
-                            <i class="fas fa-check"></i>
+                            <i class="fas fa-check" style="color: #6990f2;"></i>
                           </div>
                         </div>
                       </li>`;
+    fileImg[index].src = image;
+    fileImg[index].classList.add("d-block");
+    fileImg[index].classList.remove("d-none");
+  } else {
+    uploadedHTML = `<li class="row" style="background: #ff99ba;">
+                        <div class="col-12">
+                          <div class="content">
+                            <div class="details">
+                              <span class="name">${name}</span>
+                            </div>
+                            <i class="fa-solid fa-xmark" style="color: #DC143C;"></i>
+                          </div>
+                        </div>
+                      </li>`;
+    fileImg[index].classList.add("d-none");
+    fileImg[index].classList.remove("d-block");
+  }
   uploadedAreas[index].innerHTML = ""; // 清空对应的 uploadedArea 的内容
   uploadedAreas[index].insertAdjacentHTML("afterbegin", uploadedHTML);
 }
@@ -111,21 +132,25 @@ addCard.addEventListener("click", function () {
   var newContent = `
       <div class="row px-3">
         <div class="col-md-6 d-flex justify-content-center">
-          <div class="card text-center" style="width: 18rem; height: 11rem; cursor: pointer;">
+          <div class="card text-center" style="width: 18rem; cursor: pointer;">
             <div class="card-file card-body d-flex flex-column justify-content-center align-items-center" style="height: 11rem; background: #f6f6f6; flex: unset;">
               <input type="file" class="file-input" hidden />
               <i class="fa-regular fa-hand-pointer mb-2" style="font-size: 3.5em; color: #3b3b3b96"></i>
-              <a href="#" class="btn btn-primary rounded-0 border-0" style="background-color: #3a95ce">上傳財力證明</a>
+              <a href="javascript:;" class="btn btn-primary rounded-0 border-0" style="background-color: #3a95ce">上傳財力證明</a>
+              <img src="" class="file-img img-fluid position-absolute p-3 d-none" alt="...">
             </div>
+            <div class="uploaded-area"></div>
           </div>
         </div>
         <div class="col-md-6 d-flex justify-content-center">
-          <div class="card text-center" style="width: 18rem; height: 11rem; cursor: pointer;">
+          <div class="card text-center" style="width: 18rem; cursor: pointer;">
             <div class="card-file card-body d-flex flex-column justify-content-center align-items-center" style="height: 11rem; background: #f6f6f6; flex: unset;">
               <input type="file" class="file-input" hidden />
               <i class="fa-regular fa-hand-pointer mb-2" style="font-size: 3.5em; color: #3b3b3b96"></i>
-              <a href="#" class="btn btn-primary rounded-0 border-0" style="background-color: #3a95ce">上傳財力證明</a>
+              <a href="javascript:;" class="btn btn-primary rounded-0 border-0" style="background-color: #3a95ce">上傳財力證明</a>
+              <img src="" class="file-img img-fluid position-absolute p-3 d-none" alt="...">
             </div>
+            <div class="uploaded-area"></div>
           </div>
         </div>
       </div>
@@ -133,4 +158,105 @@ addCard.addEventListener("click", function () {
 
   // 在指定的元素前插入新的HTML内容
   addCard.insertAdjacentHTML("beforebegin", newContent);
+
+  cardFiles = document.querySelectorAll(".card-file");
+  fileInputs = document.querySelectorAll(".file-input");
+  uploadedAreas = document.querySelectorAll(".uploaded-area");
+  fileImg = document.querySelectorAll(".file-img");
+
+  let lastTwoCardFiles = Array.from(cardFiles).slice(-2);
+  let lastTwoFileInputs = Array.from(fileInputs).slice(-2);
+  let lastTwoUploadedAreas = Array.from(uploadedAreas).slice(-2);
+  let lastTwoFileImg = Array.from(fileImg).slice(-2);
+
+  lastTwoCardFiles.forEach((cardFile, index) => {
+    cardFile.addEventListener("click", () => {
+      lastTwoFileInputs[index].click();
+    });
+  });
+
+  lastTwoFileInputs.forEach((fileInput, index) => {
+    fileInput.onchange = ({ target }) => {
+      let success = true;
+      let file = target.files[0];
+      if (file) {
+        let fileName = file.name;
+        if (fileName.length >= 11) {
+          let splitName = fileName.split(".");
+          splitName = splitName[0].substring(0, 11) + "... ." + splitName[1];
+          fileName = splitName;
+        }
+
+        let fileSize = file.size;
+        let fileSizeString = "";
+
+        if (fileSize < 1024) {
+          fileSizeString = fileSize + " B"; // 小于1KB，以字节为单位
+        } else if (fileSize < 1024 * 1024) {
+          fileSizeString = (fileSize / 1024).toFixed(2) + " KB"; // 小于1MB，以KB为单位，保留两位小数
+        } else if (fileSize < 30 * 1024 * 1024) {
+          fileSizeString = (fileSize / (1024 * 1024)).toFixed(2) + " MB"; // 小于30MB，以MB为单位，保留两位小数
+        } else {
+          fileName = "文件大小超过30MB";
+          success = false;
+        }
+
+        // 检查文件格式和文件名
+        let allowedFormats = ["jpg", "jpeg"];
+        let fileFormat = fileName.split(".").pop().toLowerCase();
+
+        if (!allowedFormats.includes(fileFormat)) {
+          fileName = "上传文件格式限制为 JPG、JPEG";
+          success = false;
+        }
+
+        let imageURL = URL.createObjectURL(file); // 获取图像的 URL
+
+        NewUploadFile(fileName, fileSizeString, imageURL, index, success);
+      }
+    };
+  });
+
+  function NewUploadFile(name, size, image, index, success) {
+    let uploadedHTML;
+    if (success) {
+      uploadedHTML = `<li class="row" style="background: #e9f0ff;">
+      <div class="col-2"><i class="fas fa-file-alt"></i></div>
+      <div class="col-10">
+        <div class="content">
+          <div class="details">
+            <span class="name">${name}．Uploaded</span>
+            <span class="size">${size}</span>
+          </div>
+          <i class="fas fa-check" style="color: #6990f2;"></i>
+        </div>
+      </div>
+    </li>`;
+
+      lastTwoFileImg[index].src = image;
+      lastTwoFileImg[index].classList.add("d-block");
+      lastTwoFileImg[index].classList.remove("d-none");
+    } else {
+      uploadedHTML = `<li class="row" style="background: #ff99ba;">
+                          <div class="col-12">
+                            <div class="content">
+                              <div class="details">
+                                <span class="name">${name}．Uploaded</span>
+                              </div>
+                              <i class="fas fa-check" style="color: #DC143C;"></i>
+                            </div>
+                          </div>
+                        </li>`;
+      lastTwoUploadedAreas[index].innerHTML = ""; // 清空对应的 uploadedArea 的内容
+      lastTwoUploadedAreas[index].insertAdjacentHTML(
+        "afterbegin",
+        uploadedHTML
+      );
+      lastTwoFileImg[index].classList.add("d-none");
+      lastTwoFileImg[index].classList.remove("d-block");
+    }
+
+    lastTwoUploadedAreas[index].innerHTML = ""; // 清空对应的 uploadedArea 的内容
+    lastTwoUploadedAreas[index].insertAdjacentHTML("afterbegin", uploadedHTML);
+  }
 });
